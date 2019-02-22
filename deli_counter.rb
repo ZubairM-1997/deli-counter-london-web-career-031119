daf400dc2ab635ff075134aba47feccfe5407351
placid-computer-9089 <zubairmaqsood866@gmail.com>
2019-02-22 22:03:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,6 +20,7 @@ def now_serving()
     puts "There is nobody waiting to be served!"
   else if line_length != 0 
     while i < line_length
+    katz_deli.shift()
     puts "Currently serving #{katz_deli[i]}"
     k
     i += 1 
